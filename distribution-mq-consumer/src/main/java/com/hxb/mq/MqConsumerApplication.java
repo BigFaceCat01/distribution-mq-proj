54a1408dcc8de68be67fad1bd9385ad36644a5bd
huangxb <18228033207@163.com>
2019-04-17 11:19:39 +0800

    初始化工程

---
+++ distribution-mq-consumer/src/main/java/com/hxb/mq/MqConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.hxb.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Created by huang xiao bao
+ * @date 2019-04-17 11:09:40
+ */
+@SpringBootApplication
+public class MqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MqConsumerApplication.class);
+    }
+}
